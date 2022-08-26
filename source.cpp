09dced5f2ec50023dfb5a52aea935dfecfb05002
Viktor <Cezn.Vi@gmail.com>
2022-08-26 21:52:53 +0300

    v0.1

---
+++ source.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include "Queue.h"
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::cout << "Демонстрація роботи переробленої черги\n";
+
+	PriorityQueue<int> q = { 1,2,3 };
+    q.print();
+    q.enqueue(10, PRIORITY::MEDIUM);
+    q.enqueue(20, PRIORITY::HIGH);
+    q.enqueue(30, PRIORITY::HIGH);
+    q.print();
+    q.dequeue();
+    q.dequeue();
+    q.print();
+
+    return 0;
+}
